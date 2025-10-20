938f2de310725dcbd81c6ce87bf92660b7f3a94e
JustMrArgus <argusprogramming@protonmail.com>
2025-10-21 02:13:23 +0300

    Optimize for github pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+const repoName = "todo-app-2";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === "production";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true,
+  },
+
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
